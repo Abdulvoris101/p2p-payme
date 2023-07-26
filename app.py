6c6591f90de923f3badbc0fda87cb66a8786ea86
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-07-27 01:24:09 +0500

    FINISH: finished authentication

---
+++ app.py
@@ -0,0 +1,10 @@
+from auth.client import Authenticator
+
+
+auth = Authenticator()
+
+auth.set_credentials("YOUR_PHONE", "YOUR_PASSWORD")
+
+verification_code = input("Verif code: ")
+
+deviceId = auth.activate(verification_code)
