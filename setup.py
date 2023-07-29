ec3978045a201324f8af2000258d1949a696cd23
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-07-29 22:01:32 +0500

    Increased release version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_long_description():
 
 setup(
     name='p2p-payme',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'annotated-types==0.5.0',
